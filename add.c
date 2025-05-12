aaeefb1aa3f1a306c8ddfc4f488eed04e1360924
Priyadarshini2k4 <priyadarshini2k4@gmail.com>
2025-05-12 15:56:33 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+    int num1,num2,sum;
+    printf("Enter two integer values:");
+    scanf("%d%d",&num1,&num2);
+    sum=num1+num2;
+    printf("sum :%d\n",sum);
+    return 0;
+}
