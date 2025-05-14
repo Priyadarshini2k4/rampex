ba99f0a1810ebe6541b52b63e91033429e6549c1
Priyadarshini2k4 <priyadarshini2k4@gmail.com>
2025-05-14 10:59:22 +0530

    Add files via upload

---
+++ arithmetic.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,sum,sub,mul,divd;
+    printf("Enter the value:");
+    scanf("%d %d",&a,&b);
+    sum=a+b;
+    sub=a-b;
+    mul=a*b;
+    divd=a/b;
+    printf("sum:%d\n",sum);
+    printf("sub:%d\n",sub);
+    printf("mul:%d\n",mul);
+    printf("div:%d\n",divd);
+    return 0;
+}
+
+
