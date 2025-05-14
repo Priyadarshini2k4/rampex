cca7b22fbac4b07c8ea3baa3c5d495233f5516c0
Priyadarshini2k4 <priyadarshini2k4@gmail.com>
2025-05-14 11:06:01 +0530

    Add files via upload

---
+++ Day1.C/celsius.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int choice;
+    printf("Celsius to fahrenheit \n");
+    printf("Farenheit to celsiius \n");
+    printf("Enter choice");
+    scanf("%d,&choice");
+    if(choice-1)
+    {
+        printf("Enter the temperature in celsius:");
+        scanf("&f");
+        (9/5+32);
+        printf("Temperature in fahrenheit is:&,2f");
+    }
+    return 0;
+}
